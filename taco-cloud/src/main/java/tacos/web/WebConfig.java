21ea5d40b42d05cb599ffab07565ae61f724d247
z-a <zeyadatef@gmail.com>
2018-11-04 14:07:17 -0500

    Added validtions for the domain objects and deleted home controller. HomeCOntroller is replaced with

---
+++ taco-cloud/src/main/java/tacos/web/WebConfig.java
@@ -0,0 +1,35 @@
+package tacos.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration //@Configuration classes are typically bootstrapped using either AnnotationConfigApplicationContext or its web-capable variant
+public class WebConfig implements WebMvcConfigurer {
+	
+	//https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/config/annotation/WebMvcConfigurer.html
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		
+		/*The addViewControllers() method is given a ViewControllerRegistry that you
+          can use to register one or more view controllers. Here, you call addViewController()
+          on the registry, passing in "/", which is the path for which your view controller will
+          handle GET requests.That method returns a ViewControllerRegistration object,
+          on which you immediately call setViewName() to specify home as the view that a
+          request for "/" should be forwarded to. And just like that, you’ve been able to replace*/
+		
+		  /*We could have added the same view controller declaration to the 
+		   * bootstrap TacoCloudApplication*/
+		
+	      registry.addViewController("/").setViewName("home");
+	}
+	
+	
+	
+
+}
+
+
+
+/*Deleted the below home controller and implemented Webconf....*/
+/*public class HomeController {	@GetMapping("/") public String home() {return "home";} }*/
