9ddd44dc8faabde8881f0e554bb0b8124d1fff3e
z-a <zeyadatef@gmail.com>
2019-01-26 13:36:44 -0500

    remove jdbc implmenentations and replace with JPA

--- taco-cloud/src/main/java/tacos/data/TacoRepository.java
+++
@@ -1,9 +0,0 @@
-package tacos.data;
-import tacos.Taco;
-
-/*Saving a taco design requires that you also
-save the ingredients associated with that taco to the Taco_Ingredients table.
-*/
-public interface TacoRepository {
-	Taco save(Taco design);
-}
